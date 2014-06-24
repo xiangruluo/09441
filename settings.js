7abfbbab7553c0d0fc06d8a8b644ae76cb48d804
feivorid <feivorid@163.com>
2014-06-24 14:45:10 +0800

    build a website

---
+++ settings.js
@@ -0,0 +1,35 @@
+module.exports = {
+    id: '09441',
+    name: 'nmblog',
+    creator: 'feivorid',
+    secretKey: 'feivorid',
+    mysql:{
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database:'09441',
+        port: 3306
+    },
+    redis:{
+        host: 'localhost',
+        port: 6379
+    },
+    session: {
+        expires: 60 // minutes
+    },
+    logging: {
+        reloadSecs: 0, //INFO: set 0 could let nodeunit tests which use log4js exit properly
+        level: 'DEBUG'
+    },
+    file: {
+        public: 'public',
+        components: 'public/components',
+        upload: 'public/upload'
+    },
+    resources: {
+        appName: '学路',
+        appTitle: '学路',
+        appCreator: '新东方',
+        errorUnknown: '不好意思，系统出了点小问题'
+    }
+};
