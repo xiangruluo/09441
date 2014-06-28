20be17eaed14f52ad47626d7558201bdf5f242d2
unknown <@feivorid.(none)>
2014-06-28 08:33:59 +0800

    fei

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3001);
 
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 //app.use(favicon());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
