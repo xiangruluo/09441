7a2223b8eb809d153e83ac307d4923ddf4ec169b
feivorid <feivorid@163.com>
2014-06-30 16:17:57 +0800

    6.30 fei

--- common/middleware/loadJsCss.js
+++ common/middleware/loadJsCss.js
@@ -1,7 +1,7 @@
 //为页面注入css和js
 var hander = function (req, res) {
     res.locals.asset = {
-        mainCss: '/build/css/main.min.css',
+        mainCss: '/build/css/main.css',
         mainJs: '/build/js/main.min.js'
     };
 };
