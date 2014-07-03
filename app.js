4e74f5893865703e71ce3c86307810a3852eb5dd
feivorid <feivorid@163.com>
2014-07-03 17:39:19 +0800

    fei 7.03;

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ routes(app);
 //require('./app/routes')(app);
 
 app.set('title', '09441');
-// view engine setup
+//模板引擎
 app.set('views', path.join(__dirname + '/app/', 'views'));
 app.set('view engine', 'ejs');
 app.set('port', 3001);
@@ -33,9 +33,9 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/// catch 404 and forward to error handler
+//扑捉404页面
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('没有页面，逗比。');
     err.status = 404;
     next(err);
 });
