98da3d7e66b74a2ca6091464b6e69db110417139
feivorid <feivorid@163.com>
2014-07-10 11:32:33 +0800

    fei

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,8 +1,18 @@
 var logger = require('../../common/log/logging').logger;
 var User = require('../models').User;
+var crypto = require('crypto');
+
+//加密方法
+var MD5 = function(password) {
+    var md5 = crypto.createHash('md5');
+    md5.update(password);
+    password = md5.digest('hex');
+    return password;
+};
 
 module.exports = function (app) {
     var loadJsCss = require('../../common/middleware/loadJsCss');
+
     //加载注册页面
     app.get('/signin',function(req, res) {
         loadJsCss(req, res);
@@ -14,7 +24,7 @@ module.exports = function (app) {
     //实现注册
     app.post('/signin/save',function(req, res) {
         var email = req.body.email;
-        var password = req.body.password.trim();
+        var password = MD5(req.body.password.trim());
         User.addAndSave(email,password,function(err) {
             if(err) {
                 logger.log(err);
@@ -48,5 +58,24 @@ module.exports = function (app) {
             res.json(returnInfo);
         });
 
+    });
+
+    //登录
+    app.post('/login',function(req,res) {
+        var email = req.body.email;
+        var password = MD5(req.body.password);
+        User.login(email,password,function(err,user) {
+            if(err) {
+                logger.log(err);
+            }
+            //console.log(user);
+            if(user.length > 0) {
+                console.log('login success!');
+                res.redirect('/');
+            }else {
+                console.log('login failed!');
+                res.redirect('/');
+            }
+        });
     });
 };
